8b4374f733fd858401fd2fb3770679fb31097d1d
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-07-05 14:42:08 +0500

    rectangle

---
+++ 6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int N;
+  scanf("%d", &N);
+  printf("%d%d%d%d%d%d\n", N, N,N,N,N,N);
+  printf("%d    %d\n", N,N);
+  printf("%d    %d\n", N,N);
+  printf("%d%d%d%d%d%d", N,N,N,N,N,N);
+  return 0;
+}
