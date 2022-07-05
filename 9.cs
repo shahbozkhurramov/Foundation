c91e6f06e8b7815e2fbd22949a453a0910f60965
shahbozkhurramov <shahbozkhurramov@gmail.com>
2022-07-05 14:43:45 +0500

    factorial

---
+++ 9.cs
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("%d!=%d\n",1, 1*1);
+  printf("%d!=%d\n",2,2*1);
+  printf("%d!=%d\n",3,3*2*1);
+  printf("%d!=%d\n",4, 4*3*2*1);
+  printf("%d!=%d\n",5, 5*4*3*2*1);
+  return 0;
+}
